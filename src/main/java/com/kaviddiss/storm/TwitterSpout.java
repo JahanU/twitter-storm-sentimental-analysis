7046a2e72707a6bb694fa1fc4fcc159851fc1ce1
Jahan <34219176+JahanUoL@users.noreply.github.com>
2020-12-30 15:28:13 +0000

    Add files via upload

---
+++ src/main/java/com/kaviddiss/storm/TwitterSpout.java
@@ -0,0 +1,121 @@
+/**
+ * Taken from the storm-starter project on GitHub
+ * https://github.com/nathanmarz/storm-starter/ 
+ */
+package com.kaviddiss.storm;
+
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import backtype.storm.Config;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+/**
+ * Reads Twitter's sample feed using the twitter4j library.
+ * 
+ * @author davidk
+ */
+@SuppressWarnings({ "rawtypes", "serial" })
+public class TwitterSpout extends BaseRichSpout {
+
+	private SpoutOutputCollector collector;
+	private LinkedBlockingQueue<Status> queue;
+	private TwitterStream twitterStream;
+
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		queue = new LinkedBlockingQueue<Status>(1000);
+		this.collector = collector;
+
+		StatusListener listener = new StatusListener() {
+			@Override
+			public void onStatus(Status status) {
+				queue.offer(status);
+			}
+
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice sdn) {
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int i) {
+			}
+
+			@Override
+			public void onScrubGeo(long l, long l1) {
+			}
+
+			@Override
+			public void onStallWarning(StallWarning stallWarning) {
+			}
+
+			@Override
+			public void onException(Exception e) {
+			}
+		};
+
+		FilterQuery filterQuery = new FilterQuery();
+		// Below is the old FilterQueries I used
+//		String[] findTheseWords = { "COVID19", "Covid", "Vaccine", "COVIDVaccine", "Pfizer", "BioNTech" };
+		String[] findTheseWords = { "Vaccine", "Pfizer", "BioNTech" };
+
+		filterQuery.track(findTheseWords);
+		String[] onlyTheseLanguages = { "en" }; // Filter by English language
+		filterQuery.language(onlyTheseLanguages);
+
+		TwitterStreamFactory factory = new TwitterStreamFactory();
+		twitterStream = factory.getInstance();
+		twitterStream.addListener(listener);
+		twitterStream.sample();
+		twitterStream.filter(filterQuery);
+	}
+
+	@Override
+	public void nextTuple() {
+		Status ret = queue.poll();
+		if (ret == null) {
+			Utils.sleep(50);
+		} else {
+			collector.emit(new Values(ret));
+		}
+	}
+
+	@Override
+	public void close() {
+		twitterStream.shutdown();
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		Config ret = new Config();
+		ret.setMaxTaskParallelism(1);
+		return ret;
+	}
+
+	@Override
+	public void ack(Object id) {
+	}
+
+	@Override
+	public void fail(Object id) {
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("tweet"));
+	}
+
+}
